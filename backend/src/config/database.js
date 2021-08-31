479008d9528d35b51612078a018ceb899078e157
Deyvison-Fernandes <deyvison_fernandes@hotmail.com>
2021-08-31 03:27:31 -0300

    Atualização conexão com o banco
    Atualização conexão com o banco

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/cadastropessoas');
+mongoose.connect('mongodb://mongo/cadastropessoas');
 
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
